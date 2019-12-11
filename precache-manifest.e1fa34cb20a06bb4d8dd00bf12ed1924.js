7e8941599171ecaf1df69061dc4b857909d86760
AliceDraghici97 <alicedraghici597@gmail.com>
2019-12-11 13:25:35 +0200

    Updates

---
+++ precache-manifest.e1fa34cb20a06bb4d8dd00bf12ed1924.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9044a1679244213c4c35eae5b650c07",
+    "url": "/monstersRolodex/index.html"
+  },
+  {
+    "revision": "6769977950123dc427a1",
+    "url": "/monstersRolodex/static/css/main.62f491e2.chunk.css"
+  },
+  {
+    "revision": "5ffb6fab3c6fb8afc674",
+    "url": "/monstersRolodex/static/js/2.f6373902.chunk.js"
+  },
+  {
+    "revision": "6769977950123dc427a1",
+    "url": "/monstersRolodex/static/js/main.e52ea592.chunk.js"
+  },
+  {
+    "revision": "43af23609d524b3c6df3",
+    "url": "/monstersRolodex/static/js/runtime-main.876baf4d.js"
+  }
+]);
